cbefe5d4047443dbddb7f06aade3b7e560287b32
dmarell <daniel.marell@gmail.com>
2017-08-23 07:33:59 +0200

    Unit test. usage example updated.

--- test/usage-example-custom-metrics.js
+++ test/usage-example-custom-metrics.js
@@ -1,16 +1,13 @@
 const StackMetrics = require('../lib/stack-metrics')
 
 const test = async () => {
-  const metrics = new StackMetrics('caglabs-155116', 'testapp', 'dev', 'testapp', 5000)
+  const metrics = new StackMetrics('../service-account.json', 'google-project-id', 'testapp', 'dev', 'testapp', 5000)
   const myValueMetric = metrics.createMetric('myValue', 'My test value', StackMetrics.TYPE_INT64)
   const myRateMetric = metrics.createMetric('myRate', 'My test value, a rate/min', StackMetrics.TYPE_RATE_PER_MINUTE)
 
-  let myValue = 0
-
   setInterval(() => {
-      console.log('Usage: write myValue:', myValue)
-      myValueMetric.write(myValue)
-      myRateMetric.write(myValue)
+      myValueMetric.writeCount(1)
+      myRateMetric.writeRate(1)
       myValue++
     },
     1000)
