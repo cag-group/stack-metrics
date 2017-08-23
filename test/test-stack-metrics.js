cccd6cf4f91f28c4c5848aad4912a81471e9aa62
dmarell <daniel.marell@gmail.com>
2017-08-23 16:44:48 +0200

    Fixed bug "timestamp undefined". Added unit tests. Added defensive code.

--- test/test-stack-metrics.js
+++ test/test-stack-metrics.js
@@ -1,6 +1,6 @@
 const StackMetrics = require('../lib/stack-metrics.js')
 const sinon = require('sinon')
-require('should')
+const should = require('should')
 
 const START_TIMESTAMP = 10000
 const SEND_INTERVAL = 2000
@@ -12,14 +12,16 @@ describe('StackMetrics', () => {
     })
   }
 
+  let metrics, stubs
+
   beforeEach(() => {
-    this.stubs = []
+    stubs = []
 
-    this.metrics = new StackMetrics(undefined, 'testproject', 'testapp', 'testenv', 'testapp', 0)
-    this.metrics.setStartTimestamp(START_TIMESTAMP)
+    metrics = new StackMetrics(undefined, 'testproject', 'testapp', 'testenv', 'testapp', 0)
+    metrics.setStartTimestamp(START_TIMESTAMP)
 
-    const projectPathStub = sinon.stub(this.metrics.client, 'projectPath')
-    this.stubs.push(projectPathStub)
+    const projectPathStub = sinon.stub(metrics.client, 'projectPath')
+    stubs.push(projectPathStub)
     projectPathStub.callsFake(projectId => {
       // console.log('Called mock projectPath')
       return '/fdn/' + projectId
@@ -27,14 +29,14 @@ describe('StackMetrics', () => {
   })
 
   afterEach(() => {
-    for (let s of this.stubs) {
+    for (let s of stubs) {
       s.restore()
     }
   })
 
-  it('createMetricDescriptor', () => {
-    const stub = sinon.stub(this.metrics.client, 'createMetricDescriptor')
-    this.stubs.push(stub)
+  it('createMetricDescriptor', async () => {
+    const stub = sinon.stub(metrics.client, 'createMetricDescriptor')
+    stubs.push(stub)
     stub.callsFake(request => {
       // console.log('Called mock createMetricDescriptor, request:', JSON.stringify(request))
       request.name.should.be.exactly('/fdn/testproject')
@@ -50,8 +52,8 @@ describe('StackMetrics', () => {
       return Promise.resolve([request.metricDescriptor])
     })
 
-    const createTimeSeriesStub = sinon.stub(this.metrics.client, 'createTimeSeries')
-    this.stubs.push(createTimeSeriesStub)
+    const createTimeSeriesStub = sinon.stub(metrics.client, 'createTimeSeries')
+    stubs.push(createTimeSeriesStub)
     createTimeSeriesStub.callsFake(request => {
       // console.log('Called mock createTimeSeries')
       request.timeSeries.length.should.be.exactly(1)
@@ -62,16 +64,20 @@ describe('StackMetrics', () => {
       timeSeries.resource.labels.project_id.should.be.exactly('testproject')
     })
 
-    this.metrics.createMetric('testValue1', 'Test value', StackMetrics.TYPE_INT64)
-    return this.metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
+    const metric = metrics.createMetric('testValue1', 'Test value', StackMetrics.TYPE_INT64)
+    metric.write(1)
+    await metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
+
+    stub.called.should.be.exactly(true)
+    createTimeSeriesStub.called.should.be.exactly(true)
   })
 
   it('createTimeSeries, value type', () => {
-    const createMetricDescriptorStub = this.createMetricDescriptorStub(this.metrics.client)
-    this.stubs.push(createMetricDescriptorStub)
+    const createMetricDescriptorStub = this.createMetricDescriptorStub(metrics.client)
+    stubs.push(createMetricDescriptorStub)
 
-    const createTimeSeriesStub = sinon.stub(this.metrics.client, 'createTimeSeries')
-    this.stubs.push(createTimeSeriesStub)
+    const createTimeSeriesStub = sinon.stub(metrics.client, 'createTimeSeries')
+    stubs.push(createTimeSeriesStub)
     createTimeSeriesStub.callsFake(request => {
       // console.log('Called mock createTimeSeries')
       request.timeSeries.length.should.be.exactly(1)
@@ -81,24 +87,24 @@ describe('StackMetrics', () => {
       timeSeries.points[0].value.int64Value.should.be.exactly(6)
     })
 
-    const testValueMetric = this.metrics.createMetric('testValue1', 'Test value', StackMetrics.TYPE_INT64)
+    const testValueMetric = metrics.createMetric('testValue1', 'Test value', StackMetrics.TYPE_INT64)
     testValueMetric.writeCount(1)
     testValueMetric.writeCount(2)
     testValueMetric.writeCount(3)
 
-    return this.metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
+    return metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
   })
 
   it('createTimeSeries, value types, many samples', async () => {
-    const createMetricDescriptorStub = this.createMetricDescriptorStub(this.metrics.client)
-    this.stubs.push(createMetricDescriptorStub)
+    const createMetricDescriptorStub = this.createMetricDescriptorStub(metrics.client)
+    stubs.push(createMetricDescriptorStub)
 
     let test
 
-    const createTimeSeriesStub = sinon.stub(this.metrics.client, 'createTimeSeries')
-    this.stubs.push(createTimeSeriesStub)
+    const createTimeSeriesStub = sinon.stub(metrics.client, 'createTimeSeries')
+    stubs.push(createTimeSeriesStub)
     createTimeSeriesStub.callsFake(request => {
-      // console.log('Called mock createTimeSeries')
+      console.log('Called mock createTimeSeries, request:', JSON.stringify(request))
       if (test === 1) {
         request.timeSeries.length.should.be.exactly(2)
 
@@ -114,37 +120,42 @@ describe('StackMetrics', () => {
         timeSeries2.points[0].interval.endTime.seconds.should.be.exactly((START_TIMESTAMP + SEND_INTERVAL) / 1000)
         timeSeries2.points[0].value.int64Value.should.be.exactly(12)
       } else if (test === 2) {
+        throw new Error('createTimeSeries should not be called when no metric value has changed')
+      } else if (test === 3) {
         request.timeSeries.length.should.be.exactly(1)
 
         const timeSeries1 = request.timeSeries[0]
         timeSeries1.metric.type.should.be.exactly('custom.googleapis.com/testapp/testValue1')
         timeSeries1.points.length.should.be.exactly(1)
-        timeSeries1.points[0].interval.endTime.seconds.should.be.exactly((START_TIMESTAMP + SEND_INTERVAL * 2) / 1000)
+        timeSeries1.points[0].interval.endTime.seconds.should.be.exactly((START_TIMESTAMP + SEND_INTERVAL * 3) / 1000)
         timeSeries1.points[0].value.int64Value.should.be.exactly(12)
       }
     })
 
-    const testValue1Metric = this.metrics.createMetric('testValue1', 'Test value 1', StackMetrics.TYPE_INT64)
-    const testValue2Metric = this.metrics.createMetric('testValue2', 'Test value 2', StackMetrics.TYPE_INT64)
+    const testValue1Metric = metrics.createMetric('testValue1', 'Test value 1', StackMetrics.TYPE_INT64)
+    const testValue2Metric = metrics.createMetric('testValue2', 'Test value 2', StackMetrics.TYPE_INT64)
 
     test = 1
     testValue1Metric.writeCount(6)
     testValue2Metric.writeCount(12)
-    await this.metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
+    await metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
 
     test = 2
+    await metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL * 2)
+
+    test = 3
     testValue1Metric.writeCount(6)
-    return this.metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL * 2)
+    return metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL * 3)
   })
 
   it('createTimeSeries, rate types', async () => {
-    const createMetricDescriptorStub = this.createMetricDescriptorStub(this.metrics.client)
-    this.stubs.push(createMetricDescriptorStub)
+    const createMetricDescriptorStub = this.createMetricDescriptorStub(metrics.client)
+    stubs.push(createMetricDescriptorStub)
 
     let test
 
-    const createTimeSeriesStub = sinon.stub(this.metrics.client, 'createTimeSeries')
-    this.stubs.push(createTimeSeriesStub)
+    const createTimeSeriesStub = sinon.stub(metrics.client, 'createTimeSeries')
+    stubs.push(createTimeSeriesStub)
     createTimeSeriesStub.callsFake(request => {
       // console.log('Called mock createTimeSeries, request:', JSON.stringify(request))
       request.timeSeries.length.should.be.exactly(3)
@@ -176,9 +187,9 @@ describe('StackMetrics', () => {
       return Promise.resolve({})
     })
 
-    const testRate1Metric = this.metrics.createMetric('testRate1', 'Test value, a rate/sec', StackMetrics.TYPE_RATE_PER_SECOND)
-    const testRate2Metric = this.metrics.createMetric('testRate2', 'Test value, a rate/min', StackMetrics.TYPE_RATE_PER_MINUTE)
-    const testRate3Metric = this.metrics.createMetric('testRate3', 'Test value, a rate/hour', StackMetrics.TYPE_RATE_PER_HOUR)
+    const testRate1Metric = metrics.createMetric('testRate1', 'Test value, a rate/sec', StackMetrics.TYPE_RATE_PER_SECOND)
+    const testRate2Metric = metrics.createMetric('testRate2', 'Test value, a rate/min', StackMetrics.TYPE_RATE_PER_MINUTE)
+    const testRate3Metric = metrics.createMetric('testRate3', 'Test value, a rate/hour', StackMetrics.TYPE_RATE_PER_HOUR)
 
     test = 1
     testRate1Metric.writeRate(1)
@@ -192,15 +203,49 @@ describe('StackMetrics', () => {
     testRate3Metric.writeRate(1 / 3600)
     testRate3Metric.writeRate(2 / 3600)
     testRate3Metric.writeRate(3 / 3600)
-    await this.metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
+    await metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
 
     test = 2
-    await this.metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL * 2)
+    await metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL * 2)
 
     test = 3
     testRate1Metric.writeRate(10)
     testRate2Metric.writeRate(10 / 60)
     testRate3Metric.writeRate(10 / 3600)
-    return this.metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL * 3)
+    return metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL * 3)
+  })
+
+  it('createTimeSeries, rate types never written', async () => {
+    const createMetricDescriptorStub = this.createMetricDescriptorStub(metrics.client)
+    stubs.push(createMetricDescriptorStub)
+
+    let test
+
+    const createTimeSeriesStub = sinon.stub(metrics.client, 'createTimeSeries')
+    stubs.push(createTimeSeriesStub)
+    createTimeSeriesStub.callsFake(request => {
+      // console.log('Called mock createTimeSeries, request:', JSON.stringify(request))
+      // Only the rate metric should be sent, not the value metric
+      request.timeSeries.length.should.be.exactly(1)
+      const timeSeries1 = request.timeSeries[0]
+      timeSeries1.points.length.should.be.exactly(1)
+      timeSeries1.metric.type.should.be.exactly('custom.googleapis.com/testapp/testRate1')
+      timeSeries1.points[0].value.doubleValue.should.be.exactly(0)
+      if (test === 1) {
+        timeSeries1.points[0].interval.endTime.seconds.should.be.exactly(12)
+      } else if (test === 2) {
+        timeSeries1.points[0].interval.endTime.seconds.should.be.exactly(14)
+      }
+      return Promise.resolve({})
+    })
+
+    metrics.createMetric('testValue1', 'Test value', StackMetrics.TYPE_INT64)
+    metrics.createMetric('testRate1', 'Test rate value, a rate/sec', StackMetrics.TYPE_RATE_PER_SECOND)
+
+    test = 1
+    await metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL)
+
+    test = 2
+    return metrics.sendValues(START_TIMESTAMP + SEND_INTERVAL * 2)
   })
 })
